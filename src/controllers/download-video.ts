a2f679614922d99146323419526753365c9070df
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-02-06 12:08:47 +0100

    chore(Root): Corrected typo in download success message
    - Updated the message 'Downloaded completed successfully' to 'Download completed successfully' for correct spelling.

--- src/controllers/download-video.ts
+++ src/controllers/download-video.ts
@@ -1,30 +1,30 @@
 interface DownloadControllerProps {
-  url: string
-  downloadVideo: boolean
+	url: string
+	downloadVideo: boolean
 }
 
 interface DownloadControllerResult {
-  success: boolean
-  errorMessage?: string
-  responseMessage?: string
+	success: boolean
+	errorMessage?: string
+	responseMessage?: string
 }
 
 export async function downloadController({
-  url,
-  downloadVideo
+	url,
+	downloadVideo
 }: DownloadControllerProps): Promise<DownloadControllerResult> {
-  try {
-    return {
-      success: true,
-      responseMessage: "Downloaded completed successfully"
-    }
-  } catch (error) {
-    console.error(error)
+	try {
+		return {
+			success: true,
+			responseMessage: "Download completed successfully"
+		}
+	} catch (error) {
+		console.error(error)
 
-    return {
-      success: false,
-      errorMessage:
-        "An error occurred while downloading the video/audio, please try again later, if the problem persists please contact me."
-    }
-  }
+		return {
+			success: false,
+			errorMessage:
+				"An error occurred while downloading the video/audio, please try again later, if the problem persists please contact me."
+		}
+	}
 }
