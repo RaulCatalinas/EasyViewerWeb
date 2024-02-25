1003f5c79e3cfe94e1bf1334b4a97ab84956c5b5
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-02-25 20:29:26 +0100

    feat(Root): Utilizing shareAPI in shareController
    - Updated shareController to utilize shareAPI for sharing functionality.
    
    - Implemented fallback mechanism to copy page URL to user clipboard if shareAPI is not supported by the browser.

--- src/controllers/share.ts
+++ src/controllers/share.ts
@@ -7,23 +7,37 @@ import { notify } from "@/notifications/notify"
 // i18n
 import { getJson, getLangFromUrl } from "@/i18n/utils"
 
+// Constants
+import { SHARE_DATA } from "@/constants/share"
+
 export async function shareController() {
 	const url = new URL(location.href)
 	const lang = getLangFromUrl(url)
 	const { share } = getJson(lang)
 
-	try {
-		await copyTextToClipboard(location.href)
+	const canShare = navigator.canShare(SHARE_DATA)
 
-		notify({
-			text: share.success,
-			type: "success"
-		})
-	} catch (error) {
-		console.error(error)
-		notify({
-			text: share.error,
-			type: "error"
-		})
+	if (canShare) {
+		try {
+			await navigator.share(SHARE_DATA)
+		} catch (error) {
+			console.error(error)
+		}
+	} else {
+		try {
+			await copyTextToClipboard(location.href)
+
+			notify({
+				text: share.success,
+				type: "success"
+			})
+		} catch (error) {
+			console.error(error)
+
+			notify({
+				text: share.error,
+				type: "error"
+			})
+		}
 	}
 }
