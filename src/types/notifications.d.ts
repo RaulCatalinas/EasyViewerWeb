d9dd6f39e396dc79f77b0f52e22acb5674145e43
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-04-08 15:36:04 +0200

    feat(Root): Define GradientColors type for notification color control
    - Created GradientColors type to better manage the definition of notification colors.

--- src/types/notifications.d.ts
+++ src/types/notifications.d.ts
@@ -1,1 +1,9 @@
 export type NotificationType = 'success' | 'error' | 'info'
+
+export type GradientColors = Record<
+	string,
+	Record<
+		"firstColor" | "secondColor" | "thirdColor",
+		`rgba(${number},${number},${number},${number})`
+	>
+>
