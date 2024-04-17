1dd3ff84364d9b917279140da2b62e8972cb2c45
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-04-17 21:24:01 +0200

    fix(Root): Resolve server crash bug by adding g flag to regex
    - Fixed bug causing server crash due to missing g flag in regular expression.
    
    ISSUES CLOSED: #43

--- src/constants/chars.ts
+++ src/constants/chars.ts
@@ -1,10 +1,10 @@
 import type { InvalidChars } from '@/types/chars'
 
 export const INVALID_CHARS: InvalidChars = {
-  windows: /[<>:"/\\|?*\\x20\\u{20}/u]/,
-  darwin: /[:/]/,
-  linux: /\//,
-  unix: /[ /.:*?<>"|{}()!@#$%^&~;,\\]/,
-  android: /[^a-zA-Z0-9_-]/,
-  ios: /[^a-zA-Z0-9_-]/
+  windows: /[<>:"/\\|?*\\x20\\u{20}/u]/g,
+  darwin: /[:/]/g,
+  linux: /\//g,
+  unix: /[ /.:*?<>"|{}()!@#$%^&~;g,\\]/g,
+  android: /[^a-zA-Z0-9_-]/g,
+  ios: /[^a-zA-Z0-9_-]/g
 } as const
